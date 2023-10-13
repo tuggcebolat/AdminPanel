a201bdf150ac068e822346e25b8e80193f0cb2af
tuğçe bolat <tuggce20@gmail.com>
2023-10-13 15:45:58 +0300

    Add project files.

---
+++ AdminPanel/Controllers/PageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdminPanel/Controllers/Setting.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Controllers
+{
+    public class Setting : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
